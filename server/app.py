3674c570a2a0ef380c8246c74ea38dfaf83a7eac
losinyen <nachuchphilip@gmail.com>
2024-06-28 21:44:32 +0300

    1 passed

--- server/app.py
+++ server/app.py
@@ -1,25 +1,21 @@
-# server/app.py
+#!/usr/bin/env python3
 
-from flask import Flask
-from flask_migrate import Migrate
+from flask import Flask # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from flask_migrate import Migrate # type: ignore
 
 from models import db
 
-# create a Flask application instance
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 
-# configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
+
 migrate = Migrate(app, db)
 
-# initialize the Flask application to use the database
 db.init_app(app)
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,13 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from sqlalchemy import MetaData # type: ignore
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-# create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
 
-# define a model class by inheriting from db.Model.
+class Owner(db.Model):
+    __tablename__ = "owners"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique = True)
+
+    pets = db.relationship("Pet", backref="owner")
+
+    def __repr__(self):
+        return f"<Pet Owner {self.name}"
 
 
 class Pet(db.Model):
@@ -16,3 +25,8 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
